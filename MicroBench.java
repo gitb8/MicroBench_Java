424bf6afb6d0a6cae5caf6a9511ac4b8b2276a2f
Leonhard Hofer <117044404+gitb8@users.noreply.github.com>
2023-02-11 18:33:50 +0100

    Add files via upload

--- MicroBench.java
+++ MicroBench.java
@@ -1,13 +1,16 @@
-public class MicroBench {
+// import java.lang.System;
+// import java.lang.System.out;
+
+public class MicroBench {       // private class?, class?
     
     // set run repetitions
     static final int RUNS = 65536;
 
     public static void main(String[] args) {
-        long start;
-        long end;
-        float first;
-        float second;
+        long start;       //assign
+        long end;         //assign
+        float first;      //assign?
+        float second;     //assign?
     
         // bench setup
 
@@ -15,60 +18,60 @@ public class MicroBench {
 
         // /bench setup
 
-    start = System.nanoTime();
-    for (int run = 0; run < RUNS; run++) {
-        // 1st run
+        start = System.nanoTime();
+        for (int run = 0; run < RUNS; run++) {
+            // 1st run
 
-        a++;
-        // a += 1           // reverse test
+            a++;
+            // a += 1           // reverse test
 
-        // bench function
-        // func1();         //fix
-        // func2();         // reverse test
+            // bench function
+            func1();
+            // func2();         // reverse test
 
-    }   // /1st run
-    end = System.nanoTime();
-    first = (float) (end - start);
+        }   // /1st run
+        end = System.nanoTime();
+        first = (float) (end - start);
 
-    // reset bench setup
+        // reset bench setup
 
-    a = 0;
+        a = 0;
 
-    // /reset bench setup
+        // /reset bench setup
 
-    start = System.nanoTime();
-    for (int run = 0; run < RUNS; run++) {
-        // 2nd run
+        start = System.nanoTime();
+        for (int run = 0; run < RUNS; run++) {
+            // 2nd run
 
-        a += 1;
-        // a++;             // reverse test
+            a += 1;
+            // a++;             // reverse test
 
-        // bench function
-        // func2();         //fix
-        // func1();         // reverse test
+            // bench function
+            func2();
+            // func1();         // reverse test
 
-    }   // /2nd run
-    end = System.nanoTime();
-    second = (float) (end - start);
+        }   // /2nd run
+        end = System.nanoTime();
+        second = (float) (end - start);
 
-    // print result nanoseconds
-    // System.out.println("1st run: " + first / (float) RUNS + " ns");
-    // System.out.println("2nd run: " + second / (float) RUNS + " ns");
+        // print result nanoseconds
+        // System.out.println("1st run: " + first / (float) RUNS + " ns");
+        // System.out.println("2nd run: " + second / (float) RUNS + " ns");
 
-    // print result microseconds
-    System.out.println("1st run: " + first / (float) RUNS / 1000.0 + " µs");
-    System.out.println("2nd run: " + second / (float) RUNS / 1000.0 + " µs");
+        // print result microseconds
+        System.out.println("1st run: " + first / (float) RUNS / 1000.0 + " µs");
+        System.out.println("2nd run: " + second / (float) RUNS / 1000.0 + " µs");
 
-    // print result milliseconds
-    // System.out.println("1st run: " + first / (float) RUNS / 1.0e6 + " ms");
-    // System.out.println("2nd run: " + second / (float) RUNS / 1.0e6 + " ms");
+        // print result milliseconds
+        // System.out.println("1st run: " + first / (float) RUNS / 1.0e6 + " ms");
+        // System.out.println("2nd run: " + second / (float) RUNS / 1.0e6 + " ms");
 
-    // print result seconds
-    // System.out.println("1st run: " + first / (float) RUNS / 1.0e9 + " s");
-    // System.out.println("2nd run: " + second / (float) RUNS / 1.0e9 + " s");
+        // print result seconds
+        // System.out.println("1st run: " + first / (float) RUNS / 1.0e9 + " s");
+        // System.out.println("2nd run: " + second / (float) RUNS / 1.0e9 + " s");
     }
 
-    void func1() {}     // demo function
+    public static void func1() {}       // demo function
     
-    void func2() {}     // demo function
+    public static void func2() {}       // demo function
 }
